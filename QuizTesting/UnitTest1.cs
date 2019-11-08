3814d34fb9672e541d926e6c2fc910a24fc26d85
Renish Kabariya <renishkabariya.123@gmail.com>
2019-11-08 11:19:42 -0600

    Add project files.

---
+++ QuizTesting/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Testing_and_DI_Quiz;
+
+namespace QuizTesting
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var fakeDataAccess = new Mock<IDataAccess>();
+            Employee employee = new Employee();
+            fakeDataAccess.Setup(e => e.GetEmployee(It.IsAny<int>())).Returns(employee);
+            fakeDataAccess.Setup(e => e.GetEmployeeSalary(It.IsAny<int>())).Returns(23.524);
+        }
+    }
+}
---
+++ Testing_and_DI_Quiz/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing_and_DI_Quiz
+{
+    public interface IDataAccess
+    {
+        double GetEmployeeSalary(int id);
+        string GetTopThreeEmployees(int id);
+        Employee GetEmployee(int id);
+    }
+    class DataAccessLayer : IDataAccess
+    {
+        TestingAndDIQuizDBEntities1 db = new TestingAndDIQuizDBEntities1();
+
+        public Employee GetEmployee(int id)
+        {
+            return db.Employees.Find(id);
+        }
+        public double GetEmployeeSalary(int id)
+        {
+            var employee = db.Employees.Find(id);
+            return employee.Salary;
+        }
+        public string GetTopThreeEmployees(int id)
+        {
+            var topThreeEmployees = db.Employees.OrderByDescending(e => e.Salary).Take(3);
+            return topThreeEmployees.ToString();
+        }
+    }
+
+    public class SalaryBusinessLogic
+    {
+        IDataAccess dta;
+        public SalaryBusinessLogic(IDataAccess dta)
+        {
+            this.dta = dta;
+        }
+        public double GetEmployeeSalary(int id)
+        {
+            return dta.GetEmployeeSalary(id);
+        }
+        public double CalculateBonus(int id)
+        {
+            var years = Math.Truncate((DateTime.Today - dta.GetEmployee(id).Hiring_Date).TotalDays / 365);
+            var salary = dta.GetEmployee(id).Salary;
+            return ((years / 100) * salary);
+        }
+        public string GetTopThreeEmployees(int id)
+        {
+            return dta.GetTopThreeEmployees(id);
+        }
+
+        public void printTopSalariedEmloyees(int id)
+        {
+            String.Format("{0}", GetTopThreeEmployees(id));
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
